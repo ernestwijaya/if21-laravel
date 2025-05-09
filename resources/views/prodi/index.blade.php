f328245bd4ac4568fa226081ce26efb125bb05fa
ernestwijaya <ernestwijaya_2428250010@mhs.mdp.ac.id>
2025-05-09 20:36:34 +0700

    Laravel_Template

--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -1,6 +1,61 @@
-<h1>Program Studi</h1>
+@extends('main')
 
-@foreach ($prodi as $item)
-    {{  $item->nama}} {{ $item->kaprodi}} 
-    {{  $item->fakultas->nama}} <br />
-@endforeach
+@section('title', 'prodi')
+@section('content')
+
+        <!--begin::Row-->
+            <div class="row">
+              <div class="col-12">
+                <!-- Default box -->
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">Program Studi</h3>
+                    <div class="card-tools">
+                      <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-collapse"
+                        title="Collapse"
+                      >
+                        <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                        <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-remove"
+                        title="Remove"
+                      >
+                        <i class="bi bi-x-lg"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nama</th>
+                            <th>Kaprodi</th>
+                            <th>Nama Fakultas</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($prodi as $item)
+                        <tr>
+                            <td> {{  $item->nama}} </td>
+                            <td>{{ $item->kaprodi}}</td>
+                            <td>{{  $item->fakultas->nama}}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    </table>
+                    </div>
+                  <!-- /.card-body -->
+                  <div class="card-footer">Footer</div>
+                  <!-- /.card-footer-->
+                </div>
+                <!-- /.card -->
+              </div>
+            </div>
+            <!--end::Row-->
+@endsection
