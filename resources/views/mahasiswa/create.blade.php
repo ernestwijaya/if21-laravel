0b40952a730c51c6b51aed77fdca76a10aeccd32
angkyadi <ernestwijaya_2428250010@mhs.mdp.ac.id>
2025-05-27 17:23:28 +0700

    simpan

---
+++ resources/views/mahasiswa/create.blade.php
@@ -0,0 +1,11 @@
+@extends('main')
+
+@section('title','mahasiswa')
+@section('content')
+{{-- begin row --}}
+<div class = "row">
+    <div class="col-12">
+        {{-- form tambah fakultas --}}
+    </div>
+</div>
+@endsection
